6e473ed1a0c08111952bced57f0b32c53988fa2a
Tomás Pino <tomas.pino.perez03@gmail.com>
2025-09-16 23:19:09 +0100

    fix: Import fast_setup module correctly in test_fast_setup.py

--- tests/test_fast_setup.py
+++ tests/test_fast_setup.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'fast_setup')))
-from fast_setup import fast_setup
+import fast_setup.fast_setup as fast_setup
 
 import shutil
 import tempfile
